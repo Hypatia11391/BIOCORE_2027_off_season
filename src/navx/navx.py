e2f4e506f2f3ba26e2197d73c570f7012a4d08b2
B-S-31415 <bsilberman@visst.ca>
2026-07-07 18:11:07 -0700

    Translated most of java navx code to python, and added navx requirement to pyroject.toml

--- src/navx/navx.py
+++ src/navx/navx.py
@@ -1,7 +1,44 @@
-from wpimath.geometry import Rotation3d
+from wpimath.geometry import Rotation3d, Rotation2d
+from commands2 import Subsystem
+from navx import AHRS
+
+from math import radians
 
 
-class Navx:
-    # TODO: Write Navx code
+class Navx(Subsystem):
+    def __init__(self):
+        self.navx = AHRS(AHRS.NavXComType.kMXP_SPI)
+
+    def get_roll_deg(self) -> float:
+        return self.navx.getRoll()
+
+    def get_pitch_deg(self) -> float:
+        return self.navx.getPitch()
+
+    def get_heading(self) -> float:
+        return Rotation2d.fromDegrees(self.navx.getYaw())
+
     def get_full_rotation(self) -> Rotation3d:
-        pass
+        return Rotation3d(
+            radians(self.get_roll_deg()),
+            radians(self.get_pitch_deg()),
+            self.get_heading().getRadians(),
+        )
+
+    def is_calibrating(self) -> bool:
+        return self.navx.isCalibrating()
+
+    def is_connected(self) -> bool:
+        return self.navx.isConnected(0)
+
+    def reset(self) -> None:
+        self.navx.reset()
+
+    def zero_yaw(self) -> None:
+        self.navx.zeroYaw()
+
+    def get_angle_deg(self) -> float:
+        return self.navx.getAngle()
+
+    def get_rate_deg(self) -> float:
+        return self.navx.getRate()
