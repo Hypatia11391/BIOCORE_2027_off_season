d7eeddcc57cb1df428a999e4790edec1ac4fda99
HypatiaSch <visstfrc@gmail.com>
2026-07-13 14:52:10 -0700

    Inverted left wheels, and re-enabled shooter and intake -- THIS MAKES THE INTAKE MOTOR STALL, needs more debugging

--- src/robot_container.py
+++ src/robot_container.py
@@ -4,13 +4,13 @@ from wpimath.kinematics import MecanumDriveKinematics, MecanumDriveWheelPosition
 
 import src.subsystems
 from src.subsystems.drive.drive_train_mecanum import DriveTrainMecanum
-#from src.subsystems.mechanisms.intake import Intake
+from src.subsystems.mechanisms.intake import Intake
 #from src.subsystems.mechanisms.feed import Feed
 #from src.subsystems.mechanisms.kicker import Kicker
-#from src.subsystems.mechanisms.shooter import Shooter
+from src.subsystems.mechanisms.shooter import Shooter
 #from src.navx.navx import Navx
 from src.commands.drive_telop import DriveTelop
-#from src.commands.operate_telop import OperateTelop
+from src.commands.operate_telop import OperateTelop
 import src.subsystems.drive.drive_train_constants as drive_constants
 import src.constants as constants
 
@@ -34,13 +34,13 @@ class RobotContainer:
         #)
 
         self.drive = DriveTrainMecanum(None,None) #(self.pose_estimator, self.navx)
-        #self.intake = Intake()
-        #self.feed = Feed()
-        #self.kicker = Kicker()
-        #self.shooter = Shooter()
+        self.intake = Intake()
+        self.feed = None#Feed()
+        self.kicker = None#Kicker()
+        self.shooter = Shooter()
 
         self.controller_drive = Joystick(0)
-        #self.controller_operate = Joystick(1)
+        self.controller_operate = Joystick(1)
 
         self.drive.setDefaultCommand(DriveTelop(self.drive, self.controller_drive))
-        #self.shooter.setDefaultCommand(OperateTelop(self.intake, self.feed, self.kicker, self.shooter, self.controller_operate))
+        self.shooter.setDefaultCommand(OperateTelop(self.intake, self.feed, self.kicker, self.shooter, self.controller_operate))
