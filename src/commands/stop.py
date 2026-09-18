f18823f3bf1446d670b9b5473e20da9b3d68abcf
B-S-31415 <bsilberman@visst.ca>
2026-09-18 10:36:13 -0700

    Wrote turn to angle and stop command

---
+++ src/commands/stop.py
@@ -0,0 +1,57 @@
+from typing import override
+
+from commands2 import Command
+
+from src.subsystems.drive.drive_train_mecanum import DriveTrainMecanum
+from src.subsystems.mechanisms.feed import Feed
+from src.subsystems.mechanisms.intake import Intake
+from src.subsystems.mechanisms.kicker import Kicker
+from src.subsystems.mechanisms.shooter import Shooter
+
+SPEED_THRESHOLD = 1
+
+
+class StopCommand(Command):
+    def __init__(self, feed: Feed, kicker: Kicker, shooter: Shooter, drive: DriveTrainMecanum, intake: Intake):
+        self.feed = feed
+        self.kicker = kicker
+        self.shooter = shooter
+        self.drive = drive
+        self.intake = intake
+
+        self.addRequirements(
+            self.feed,
+            self.kicker,
+            self.shooter,
+            self.drive,
+            self.intake,
+        )
+
+    @override
+    def initialize(self):
+        pass
+
+    @override
+    def execute(self):
+        self.feed.stop()
+        self.kicker.stop()
+        self.shooter.stop()
+        self.drive.drive(0, 0, 0)
+        self.intake.stop()
+
+    @override
+    def end(self, interrupted: bool):
+        pass
+
+    @override
+    def isFinished(self) -> bool:
+        wheel_speeds = self.drive.get_wheel_positions()
+
+        return all(
+            [
+                abs(wheel_speeds.frontLeft) < SPEED_THRESHOLD,
+                abs(wheel_speeds.frontRight) < SPEED_THRESHOLD,
+                abs(wheel_speeds.rearLeft) < SPEED_THRESHOLD,
+                abs(wheel_speeds.rearRight) < SPEED_THRESHOLD,
+            ]
+        )
---
+++ src/commands/turn_to.py
@@ -0,0 +1,45 @@
+from typing import override
+
+from commands2 import Command
+from wpimath.controller import PIDController
+from wpimath.estimator import MecanumDrivePoseEstimator3d
+
+from src.subsystems.drive.drive_train_mecanum import DriveTrainMecanum
+
+ANGLE_THRESHOLD = 1
+
+
+class TurnToCommand(Command):
+    def __init__(self, target_angle: float, drive: DriveTrainMecanum, pose_estimator: MecanumDrivePoseEstimator3d, pid: PIDController):
+        self.target_angle = target_angle
+
+        self.drive = drive
+        self.pose_estimator = pose_estimator
+        self.pid = pid
+
+        self.addRequirements(self.drive)
+
+    @override
+    def initialize(self):
+        pass
+
+    @override
+    def execute(self):
+        pid_out = self.pid.calculate(
+            self.pose_estimator.getEstimatedPosition().rotation().angle,
+            self.target_angle,
+        )
+
+        pid_out = min(1, max(-1, pid_out))
+
+        self.drive.drive(0, 0, pid_out)
+
+    @override
+    def end(self, interrupted: bool):
+        pass
+
+    @override
+    def isFinished(self) -> bool:
+        angle = self.pose_estimator.getEstimatedPosition().rotation().angle
+
+        return abs(angle - self.target_angle) < ANGLE_THRESHOLD
